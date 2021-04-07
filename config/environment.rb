c69f1a992f9ddbd564e07dfa7b826edeb41815c8
Mike Barberry <mbarberry@Mikes-MacBook-Pro.local>
2021-04-07 16:42:17 -0700

    cosmetic

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@ require 'bundler/setup'
 Bundler.require
 
 ActiveRecord::Base.establish_connection(
-    "postgres://mbarberry@localhost/test_db"
-  )
+  "postgres://mbarberry@localhost/test_db"
+)
 
 require_all 'app'

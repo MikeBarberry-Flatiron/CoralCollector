15998c443bd1358ae7e8d288769a6789cd80280c
Mike Barberry <52507419+MikeBarberry@users.noreply.github.com>
2021-11-21 21:00:54 -0800

    changes to env.rb

--- config/environment.rb
+++ config/environment.rb
@@ -7,16 +7,7 @@ configure :development do
 end
 
 configure :production do 
-	db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
-
- 	ActiveRecord::Base.establish_connection(
-		:adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-   		:host     => db.host,
-   		:username => db.user,
-   		:password => db.password,
-   		:database => db.path[1..-1],
-   		:encoding => 'utf8'
- 	)
+ 	ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"])
 end
 
 require_all 'app'
